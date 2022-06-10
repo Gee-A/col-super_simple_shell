13dbf6a913f41eabc7b5052a97a0d1610756deb3
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-06-10 15:49:32 -0700

    shell is not rocket science if you program it

---
+++ simple_shell/basic_functions.c
@@ -0,0 +1,7 @@
+#include "holberton.h"
+
+/**
+ *  To contain all of string function
+ *  memory allocation function
+ *  and others that are common across any program
+ */
---
+++ simple_shell/sub_fuction.c
@@ -0,0 +1,6 @@
+#include "holberton.h"
+
+/**
+ * sub_functions includes all helper functions need to run the program
+ * properly and ensuring a desent and understandable program
+ */
