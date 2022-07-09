a8d17ff79f5f93972b911b3b9d485bd9922e83b3
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-07-09 03:02:00 -0700

    removed incomplete version of simple_shell

--- simple_shell/basic_functions.c
+++
@@ -1,7 +0,0 @@
-#include "holberton.h"
-
-/**
- *  To contain all of string function
- *  memory allocation function
- *  and others that are common across any program
- */
--- simple_shell/sub_functions.c
+++
@@ -1,6 +0,0 @@
-#include "holberton.h"
-
-/**
- * sub_functions includes all helper functions need to run the program
- * properly and ensuring a desent and understandable program
- */
